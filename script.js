d8abf684b1c5b7297b6043d14f225fed1aa9fe41
Max <maksimgr@ukr.net>
2020-01-26 21:06:50 +0200

    task1_2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let money = parseInt(prompt("Ваш бюджет на месяц?"));
-let time = new Date(prompt("Введите дату в формате YYYY-MM-DD"));
+let money = parseInt(prompt("Ваш бюджет на месяц?")),
+    time = new Date(prompt("Введите дату в формате YYYY-MM-DD"));
+
 let appData = {
     badget: money,
     timeData: time,
@@ -9,8 +10,8 @@ let appData = {
     saving: false
 };
 
-let expenseItem = prompt("Введите обязательную статью расходов в этом месяце");
-let expenseItemCost = parseInt(prompt("Во сколько обойдется?"));
+let expenseItem = prompt("Введите обязательную статью расходов в этом месяце"),
+    expenseItemCost = parseInt(prompt("Во сколько обойдется?"));
 appData.expenses[expenseItem] = expenseItemCost;
 
 alert(money / 30);
