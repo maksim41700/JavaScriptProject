8b3227ae0042e4cc41c4072f531263538db9cfb1
Max <maksimgr@ukr.net>
2020-02-10 21:05:58 +0200

    mobile

---
+++ mobile/script.js
@@ -0,0 +1,41 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    let box = document.querySelector('#rectangle');
+
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        console.log('It works, touchstart.');
+        console.log(e.target);
+        console.log(e.touches);
+        console.log(e.changedTouches);
+        console.log(e.targetTouches);
+
+    });
+
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+        console.log('It works, touchmove.' + e.touches[0].pageX);
+
+    });
+
+    box.addEventListener('touchend', (e) => {
+        e.preventDefault();
+        console.log('It works, touchend.');
+    })
+
+    // new RegExp('pattern', 'flags');
+
+    // let ans = prompt('fill you mane');
+
+    // let reg = /n/ig;
+
+    // // console.log(ans.search(reg));
+    // console.log(ans.match(reg));
+
+    let pass = prompt('enter password');
+    console.log(pass.replace(/./g, '*'));
+
+    alert('12-34-56'.replace(/-/g, ':'));
+
+
+})
