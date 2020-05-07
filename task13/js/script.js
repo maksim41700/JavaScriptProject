f0688420194e536a76a9cb360bcc086050daab36
Max <maksimgr@ukr.net>
2020-05-07 18:55:46 +0300

    task13

--- task13/js/script.js
+++ task13/js/script.js
@@ -38,7 +38,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //Timer
 
-    let deadLine = '2020-03-28';
+    let deadLine = '2020-08-28';
 
     function getTimeRemaining(endTime) {
         let t = Date.parse(endTime) - Date.parse(new Date()),
@@ -117,12 +117,40 @@ window.addEventListener('DOMContentLoaded', () => {
 
     statusMessage.classList.add('status');
 
-    form.addEventListener('submit', (event) => {
+    form.addEventListener('submit', function(event) {
         event.preventDefault();
         form.appendChild(statusMessage);
 
-        let request = new XMLHttpRequest('POST');
-        request.open('test');
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(form);
+
+        let obj = {};
+
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState == 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+
     })
 
 });
